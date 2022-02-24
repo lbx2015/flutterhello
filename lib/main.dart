3d87acf098182a49b2ce6a522572c9ab765517fa
zzw <zhangziwei@jkinvest.com>
2022-02-24 11:23:31 +0800

    upgrade compile sdk version

--- lib/main.dart
+++ lib/main.dart
@@ -14,14 +14,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body:  Center(
+          child: Text(wordPair.asPascalCase),
         ),
       ),
     );
